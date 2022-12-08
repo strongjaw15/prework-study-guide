6fafa4230f38e8b6665085af9e49390a7a7b8bb6
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2022-12-08 11:22:18 -0600

    added spacing in code

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
 function listTopics() {
     for(var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
     }
 }
+
 function selectTopic() {
     if (topic === 'HTML') {
     console.log("let's study HTML!");
